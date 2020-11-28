62369bb630af42d0309f609016501ec3eab3f7ac
eliot-akira <me@eliotakira.com>
2020-11-28 16:25:19 +0100

    Refactor route chunk generation

--- src/builder/dynamic.js
+++ src/builder/dynamic.js
@@ -4,6 +4,17 @@ const globby = require('globby')
 const chokidar = require('chokidar')
 const chalk = require('chalk')
 
+/**
+ * Automatic generation of route chunks from index files in src/pages
+ *
+ * Produces the following files in src/routes:
+ *
+ * - chunks.client.js
+ * - chunks.server.js
+ *
+ * They are imported from elsewhere as 'chunks', and the builder aliases the corresponding
+ * variant for client and server bundles.
+ */
 module.exports = async function() {
 
   const cwd = process.cwd()
@@ -12,16 +23,13 @@ module.exports = async function() {
 
   const indexGlob = path.join(pagesDir, '**/{index,404}.{js,tsx,mdx}')
 
-  // Clear cache before build, to catch generated code
-  await fs.emptyDir('node_modules/.cache/babel-loader')
-
   const generate = async () => {
 
     const allIndexes = await globby(
       indexGlob
     )
 
-    const addFileToMap = (map, file) => {
+    const exportedMap = allIndexes.reduce((map, file) => {
 
       const fileBase = file.split('/').pop()
       const extension = fileBase.split('.').pop()
@@ -33,34 +41,27 @@ module.exports = async function() {
 
       if (fileBase.split('.')[0]==='404') route = '404'
 
-      if (map[ route ]) {
-        if (extension==='mdx') return map
-      }
+      // mdx can be overridden by js, tsx
+      if (map[ route ] && extension==='mdx') return map
 
       map[ route ] = {
         file: relativeFile,
         extension
       }
+
       return map
-    }
+    }, {})
 
-    const exportedMap = allIndexes.reduce(addFileToMap, {})
-    const createChunksString = type => (map) => Object.keys(map).map(route =>
-      type==='client'
-        // Client - Dynamic import
-        ? `'${route}': load(() => import(/* webpackChunkName: "${route}" */ '${
-          exportedMap[route].file
-        }'))`
-        // Server - Require (and bundle all)
-        : `'${route}': load('${route}', require('${
-          exportedMap[route].file
-        }'))`
-    ).join(',\n  ')
-
-    // console.log(chunks)
+    /**
+     * Client - Dynamic import
+     */
 
     const chunksClient = path.join(routesDir, 'chunks.client.js')
-    const createClientChunks = createChunksString('client')
+    const createClientChunks = map => Object.keys(map).map(route =>
+      `'${route}': load(() => import(/* webpackChunkName: "${route}" */ '${
+        exportedMap[route].file
+      }'))`
+    ).join(',\n  ')
 
     await fs.writeFile(chunksClient, `// Generated - Client-side routes
 
@@ -71,9 +72,16 @@ export default {
 }
 `, 'utf8')
 
+    /**
+     * Server - Require and bundle all
+     */
 
     const chunksServer = path.join(routesDir, 'chunks.server.js')
-    const createServerChunks = createChunksString('server')
+    const createServerChunks = map => Object.keys(map).map(route =>
+      `'${route}': load('${route}', require('${
+        exportedMap[route].file
+      }'))`
+    ).join(',\n  ')
 
     await fs.writeFile(chunksServer, `// Generated - Server-side routes
 
@@ -91,6 +99,10 @@ export default {
 
   if (process.env.NODE_ENV!=='development') return
 
+  /**
+   * Dev mode - Watch files and regenerate routes
+   */
+
   // https://github.com/paulmillr/chokidar#api
   const watchOptions = {
     ignoreInitial: true,
@@ -104,12 +116,9 @@ export default {
   const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1)
 
   chokidar.watch(
-
-    // `${pagesDir}/**/*` // Watch folders also
-
-    indexGlob //
-
-    , watchOptions)
+    indexGlob,
+    watchOptions
+  )
     .on('all', (event, file) => {
       // Only watch for add/remove
       if (['add', 'unlink', 'change'].indexOf(event) < 0) return
